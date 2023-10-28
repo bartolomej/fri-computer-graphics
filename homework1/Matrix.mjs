b80838eafed1cb640337e82990b8397e55a11b50
Bart <bartolomej.kozorog@gmail.com>
2023-10-28 10:34:50 +0200

    add new homework template

---
+++ homework1/Matrix.mjs
@@ -0,0 +1,55 @@
+// Return matrices as 2D arrays in row-major order, e.g.:
+// return [
+//     [ 1, 2, 3, 4 ],
+//     [ 5, 6, 7, 8 ],
+//     [ 7, 6, 5, 4 ],
+//     [ 3, 2, 1, 0 ],
+// ];
+
+export function identity() {
+    // TODO implement
+}
+
+export function translation(t) {
+    // TODO implement
+}
+
+export function scaling(s) {
+    // TODO implement
+}
+
+export function rotationX(angle) {
+    // TODO implement
+}
+
+export function rotationY(angle) {
+    // TODO implement
+}
+
+export function rotationZ(angle) {
+    // TODO implement
+}
+
+export function negate(m) {
+    // TODO implement
+}
+
+export function add(m, n) {
+    // TODO implement
+}
+
+export function subtract(m, n) {
+    // TODO implement
+}
+
+export function transpose(m) {
+    // TODO implement
+}
+
+export function multiply(m, n) {
+    // TODO implement
+}
+
+export function transform(m, v) {
+    // TODO implement
+}
---
+++ homework1/Vector.mjs
@@ -0,0 +1,50 @@
+// Return vectors as arrays, e.g.:
+// return [ 1, 2, 3, 4 ];
+
+export function negate(v) {
+    // TODO implement
+}
+
+export function add(v, w) {
+    // TODO implement
+}
+
+export function subtract(v, w) {
+    // TODO implement
+}
+
+export function multiply(v, w) {
+    // TODO implement
+}
+
+export function divide(v, w) {
+    // TODO implement
+}
+
+export function dot(v, w) {
+    // TODO implement
+}
+
+export function cross(v, w) {
+    // TODO implement
+}
+
+export function length(v) {
+    // TODO implement
+}
+
+export function normalize(v) {
+    // TODO implement
+}
+
+export function project(v, w) {
+    // TODO implement
+}
+
+export function reflect(v, w) {
+    // TODO implement
+}
+
+export function angle(v, w) {
+    // TODO implement
+}
